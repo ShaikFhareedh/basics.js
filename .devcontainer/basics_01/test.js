a39c9d08b8c419cce8b0214c6ae8214a9326b5e3
shaik fhareedh <140721524+ShaikFhareedh@users.noreply.github.com>
2023-08-31 12:15:49 +0000

    variables

--- .devcontainer/basics_01/test.js
+++ .devcontainer/basics_01/test.js
@@ -1,1 +1,7 @@
 console.log("fhareedh")
+const accountId = "12345"
+let accountEmail = "fhareedhshaik8688@gmail.com"
+var accountPassword = "123456789"
+accountCity = "bangalore"
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity]);
